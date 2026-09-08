d7ab6cd83dfe2b43fafec57fdeb23c7f0f7e1a93
Raphaël Jontef <raphael.jontef@enseirb-matmeca.fr>
2026-09-08 12:22:23 +0200

    zou

--- flake.nix
+++ flake.nix
@@ -5,7 +5,20 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = with pkgs; [
+        nodejs
+        postgresql_18
+      ];
+    };
     nixosModules = {
       Recipiz = {
         imports = [
